6ea188d341f19efeaae9aff0479610e586106d09
aball6412 <aaron.thompkins12@gmail.com>
2016-06-21 23:56:17 -0400

    Full site style complete

--- particles.js
+++ particles.js
@@ -1,3 +1,11 @@
+/* -----------------------------------------------
+/* Author : Vincent Garreau  - vincentgarreau.com
+/* MIT license: http://opensource.org/licenses/MIT
+/* Demo / Generator : vincentgarreau.com/particles.js
+/* GitHub : github.com/VincentGarreau/particles.js
+/* v2.0.0
+/* ----------------------------------------------- */
+
 particlesJS.load('particles-js', 'assets/particles.json', function() {
   console.log('Successfully loaded particles.js');
 });
